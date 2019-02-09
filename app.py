8dbe75e30ba8fa44c87363153e7ed5a5b60d3c50
Theriley106 <christopherlambert106@gmail.com>
2019-02-09 10:57:10 -0500

    Added files

--- app.py
+++ app.py
@@ -2,10 +2,14 @@ from flask import Flask, render_template, request, url_for, redirect, Markup, js
 
 app = Flask(__name__, static_url_path='/static')
 
+try:
+	from keys import *
+except:
+	API_KEY = raw_input("Key: ")
 
 @app.route('/', methods=['GET'])
 def index():
-	return render_template("index.html")
+	return render_template("index.html", API_KEY=API_KEY)
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=5000)
