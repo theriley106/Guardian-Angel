ef108346934a2a22cf80dde0ef16f95406a2c191
Theriley106 <christopherlambert106@gmail.com>
2019-02-09 12:12:14 -0500

    added html

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, request, url_for, redirect, Markup, jsonify, make_response, send_from_directory, session
 import os
 is_prod = os.environ.get('IS_HEROKU', None)
-
+longitudeHistory = [None]
+latitudeHistory = [None]
 if is_prod == None:
 	try:
 		from keys import *
@@ -21,7 +22,16 @@ def get_long_lat():
 	longitude = request.args.get('long')
 	latitude = request.args.get('lat')
 	print("Long: {} | Lat: {}".format(longitude, latitude))
+	longitudeHistory.append(longitude)
+	latitudeHistory.append(latitude)
 	return jsonify({"status": True})
+
+@app.route('/guardian', methods=['GET'])
+def get_guardian():
+	#print("Long: {} | Lat: {}".format(longitude, latitude))
+	longitude = longitudeHistory
+	latitude = latitudeHistory
+	return render_template("guardian.html", LATITUDE=latitude, LONGITUDE=longitude)
 
 
 if __name__ == '__main__':
