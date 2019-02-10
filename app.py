26a3b6f6ec6b5fdc92cd029d943b2c6a5476c305
ayushpetigara <aarvyushpetigara3@gmail.com>
2019-02-10 02:36:57 -0500

    Added style to index

--- app.py
+++ app.py
@@ -15,8 +15,8 @@ app = Flask(__name__, static_url_path='/static')
 @app.route('/', methods=['GET'])
 def index():
 	# return render_template("map.html", API_KEY=API_KEY, AGORA_KEY=AGORA_KEY)
-	return render_template('index.html', API_KEY = API_KEY)
-
+	# return render_template('index.html', API_KEY = API_KEY)
+	return render_template('index.html')
 @app.route('/select', methods=['GET'])
 def select_screen():
 	# return render_template("map.html", API_KEY=API_KEY, AGORA_KEY=AGORA_KEY)
