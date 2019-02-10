007b06343aed596153db2693c9ab5bd3d9022172
Theriley106 <christopherlambert106@gmail.com>
2019-02-10 12:21:22 -0500

    Removed send text

--- sendText.py
+++
@@ -1,4 +0,0 @@
-import os
-
-message = """Chris has requested a guardian angel to overlook his walk home.  Check out: https://guardianangels.herokuapp.com/mySite?person=Chris&location=Home"""
-os.system("lib messagebird.sms.create --recipient 18646097067 --body {}".format(message))
